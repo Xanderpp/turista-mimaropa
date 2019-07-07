e28a6d58bc22876e712aed2a14080491f05e0983
unknown <alexanderpatrick.pijan@gmail.com>
2019-07-08 07:23:41 +0800

    fixed login page [dost]

--- application/views/welcome_message.php
+++ application/views/welcome_message.php
@@ -2,47 +2,6 @@
 <!DOCTYPE html>
 <html lang="en">
 	<head>
-<<<<<<< HEAD
-		<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
-		<link rel="stylesheet" href="assets/style.css">
-		<meta charset="utf-8">
-		<title>Welcome to Turista MIMAROPA!</title>
-	</head>
-	<body>
-		<nav id="nav1" class="navbar navbar-expand-lg navbar-light p-4">
-			<a class="navbar-brand" href="#">
-				<img id="logo1" src="assets/turista-mimaropa-blk.png">
-			</a>
-			<button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-			<span class="navbar-toggler-icon"></span>
-			</button>
-
-			<div class="collapse navbar-collapse" id="navbarSupportedContent">
-				<ul class="navbar-nav mr-auto">
-					<li class="nav-item active">
-					<a class="nav-link" href="#">Home <span class="sr-only">(current)</span></a>
-					</li>
-					<li class="nav-item">
-					<a class="nav-link" href="#">Link</a>
-					</li>
-					<li class="nav-item dropdown">
-					<a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-					Dropdown
-					</a>
-					<div class="dropdown-menu" aria-labelledby="navbarDropdown">
-					<a class="dropdown-item" href="#">Action</a>
-					<a class="dropdown-item" href="#">Another action</a>
-					<div class="dropdown-divider"></div>
-					<a class="dropdown-item" href="#">Something else here</a>
-					</div>
-					</li>
-					<li class="nav-item">
-					<a class="nav-link disabled" href="#" tabindex="-1" aria-disabled="true">Disabled</a>
-					</li>
-				</ul>
-			</div>
-		</nav>
-=======
 		<link rel="stylesheet" href="assets/style.css">
 		<!-- <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous"> -->
 		<link rel="stylesheet" href="https://bootswatch.com/4/sandstone/bootstrap.min.css">
@@ -54,7 +13,7 @@
 		<div id="overlay" class="text-white text-center py-4 d-flex justify-content-center align-items-center vh-100">
 			<div class="col col-6 bg-white rounded p-4">
 				<div class="">
-					<img id="logo" src="assets/turista-mimaropa.png" />
+					<img id="logo" class="img-fluid" src="assets/turista-mimaropa.png" />
 					<form>
 						<div class="text-left py-2">
 							<label class="text-secondary mb-n1">Username</label>
@@ -74,7 +33,6 @@
 				</div>
 			</div>
 		</div>
->>>>>>> c6361cfc9a903af35118e5c1a68f506337616709
 	</body>
 	<script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
 	<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
